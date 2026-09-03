9b77df41635d7e3cd970766de460af3d00c2e9cc
Nishant Gupta <nishant442201@gmail.com>
2026-09-04 03:00:57 +0530

    fix: minor changes

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -1,4 +1,11 @@
-import { createContext, useContext,useMemo, useState, useCallback } from 'react';
+import {
+  createContext,
+  useContext,
+  useMemo,
+  useState,
+  useCallback,
+  useEffect,
+} from 'react';
 import { VEHICLE_STATUS, VIEW_TYPE } from '../utils/Constant';
 const AppContext = createContext();
 
@@ -7,9 +14,30 @@ export const AppProvider = ({ children }) => {
   const [selectedVehicle, setSelectedVehicle] = useState(null);
   const [vehiclesList, setVehiclesList] = useState([]);
   const [limit, setLimit] = useState(10);
-  const [filterStatus, setFilterStatus ] = useState('');
+  const [filterStatus, setFilterStatus] = useState('');
+  const [countByStatus, setCountByStatus] = useState({
+    [VEHICLE_STATUS.ALL.key]: 0,
+    [VEHICLE_STATUS.DELIVERED.key]: 0,
+    [VEHICLE_STATUS.INACTIVE.key]: 0,
+    [VEHICLE_STATUS.EN_ROUTE.key]: 0,
+  });
 
+  useEffect(() => {
+    const counts = {
+      [VEHICLE_STATUS.ALL.key]: vehiclesList.length,
+      [VEHICLE_STATUS.DELIVERED.key]: 0,
+      [VEHICLE_STATUS.INACTIVE.key]: 0,
+      [VEHICLE_STATUS.EN_ROUTE.key]: 0,
+    };
 
+    vehiclesList.forEach((vehicle) => {
+      if (counts[vehicle.status] !== undefined) {
+        counts[vehicle.status] += 1;
+      }
+    });
+
+    setCountByStatus(counts);
+  }, [vehiclesList, setCountByStatus]);
   const handleViewChange = useCallback((view) => {
     setViewType(view);
   }, []);
@@ -19,23 +47,25 @@ export const AppProvider = ({ children }) => {
   }, []);
 
   const filteredVehicleList = useMemo(() => {
-    return filterStatus ? vehiclesList.filter(l => l.status === filterStatus): vehiclesList;
+    return filterStatus
+      ? vehiclesList.filter((l) => l.status === filterStatus)
+      : vehiclesList;
   }, [filterStatus, vehiclesList]);
   const value = {
     viewType,
     selectedVehicle,
-    vehiclesList: filteredVehicleList, setVehiclesList,
-    filterStatus, setFilterStatus,
-    limit, setLimit,
+    vehiclesList: filteredVehicleList,
+    setVehiclesList,
+    filterStatus,
+    setFilterStatus,
+    limit,
+    setLimit,
+    countByStatus,
     handleViewChange,
     handleSelectVehicle,
   };
 
-  return (
-    <AppContext.Provider value={value}>
-      {children}
-    </AppContext.Provider>
-  );
+  return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => {
